20674a54c7479e287c026db26501088188bf1b22
yskim0201 <yskim__0201@naver.com>
2022-03-04 10:16:28 +0900

    first commit

---
+++ react/example/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Excomponent from './ex_component.js'
+
+function App() {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>CSS 적용하기</p>
+            <Excomponent></Excomponent>
+        </div>
+    ) ;
+}
+
+export default App;
---
+++ react/example/src/ex_component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ex_component extends Component {
+    render() {
+        return (
+            <h2>[New ex_component]</h2>
+        )
+    }
+}
+
+export default ex_component;
